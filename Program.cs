e62d29d6e7808bd19cf85fa4d0504f70cfbb8cf1
Asif-36 <130299330+Asif-36@users.noreply.github.com>
2024-11-28 16:04:52 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("h");
+Console.WriteLine("Enter a number to count Fact number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int fact = 1;
+for (int i = 1; i <= number; i++)
+{
+    //shamim- asif
+    fact = fact * i;   //Ekhane kintu 1 hobe na, i gun hobe!!!
+
+}
+Console.WriteLine($"Factorial({number}) = {fact}");
